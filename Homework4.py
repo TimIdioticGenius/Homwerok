4d17e0387c830d5078451b143755a8a999c45f8e
TimIdioticGenius <timofey.boom230607@gmail.com>
2024-05-16 19:23:39 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,6 @@
+immutable_var=12,'mim',1.4,False
+print(immutable_var)
+#immutable_var[0]=23    выдаёт ошибку т.к. данные кортежа нельзя изменять  закоментил чтобы не удалять из кода
+mutable_list=['cat','owl','understanding']
+mutable_list[2]='snake'Homework4.py
+print(mutable_list)
