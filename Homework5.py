4578760b918fa6cf2328309a8828a45ca5040e27
TimIdioticGenius <timofey.boom230607@gmail.com>
2024-05-17 20:29:14 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,12 @@
+my_list=['apple','banana','pineapple','coconut','dragonfruit','grapefruit']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2]='pear'
+print(my_list)
+
+my_dict={'Rome':'Рим','ape':'обезьяна'}
+print(my_dict)
+my_dict[3.14]='ПИ'
+print(my_dict)
