ac1e49d5db6b685b4834d34dfa1637775223b1fd
TimIdioticGenius <timofey.boom230607@gmail.com>
2024-06-09 15:00:41 +0300

    Update module_2_7.py

--- module_2_7.py
+++ module_2_7.py
@@ -1,3 +1,6 @@
+a=0
+b=0
+c=0
 def print_params(a = 1, b = 'строка', c = True):
     print(a,b,c)
 print_params()
@@ -5,7 +8,8 @@ print_params(8)
 print_params(2,'asdads')
 print_params(True,'real',str)
 value_list = [True, 148, 'a']
-value_dict = {'true': True, '148': 'a', 'a': 148} #функция отказывается принимать словарь по неизвестным мне причинам
-#print_params(*value_list, **value_dict)
+value_dict = {'a': True, 'b': 'a', 'c': 148}
+print_params(*value_list)
+print_params(**value_dict)
 value_list2=[False,'a']
 print_params(*value_list2,42)
