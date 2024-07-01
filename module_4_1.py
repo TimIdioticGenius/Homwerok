73ebdde0328d8779ec913e254422fbb01ef23a6a
TimIdioticGenius <timofey.boom230607@gmail.com>
2024-07-01 16:39:35 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from math import inf
+import true_math
+import fake_math
+print(true_math.divide(5,2))
+print(true_math.divide(921387, 0))
+print(fake_math.divide(18,3))
+print(fake_math.divide(102389197, 0))
